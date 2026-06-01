a5f2629327841feb78cb537254c93d4447373a4b
Gabriel Valente <gabriel.valente@grenoble-inp.org>
2026-06-01 11:38:25 +0200

    traite_car() OK

--- kernel/console.c
+++ kernel/console.c
@@ -3,10 +3,15 @@
 #include "cpu.h"
 
 const uint16_t* VGABASE = (uint16_t*)0xB8000;
+uint16_t ROW = 0;
+uint16_t COL = 0;
+uint16_t ROW_MAX = 25;
+uint16_t COL_MAX = 80;
+
 
 const uint16_t* ptr_mem(uint32_t lig, uint32_t col)
 {
-    return VGABASE + (lig * 80 + col);
+    return VGABASE + (lig * COL_MAX + col);
 }
 
 /*
@@ -24,7 +29,7 @@ void ecrit_car(uint32_t lig, uint32_t col, char c, uint8_t ct, uint8_t cf, bool 
 
 void place_curseur(uint32_t lig, uint32_t col)
 {
-    uint16_t pos = lig * 80 + col;
+    uint16_t pos = lig * COL_MAX + col;
       
     // indiquer à la carte que l'on va envoyer la partie basse de la position du curseur
     outb(0x0F, 0x3D4);
@@ -38,11 +43,67 @@ void place_curseur(uint32_t lig, uint32_t col)
 
 void efface_ecran(void)
 {
-    for (uint32_t lig = 0; lig < 25; lig++) {
-        for (uint32_t col = 0; col < 80; col++) {
+    for (uint32_t lig = 0; lig < ROW_MAX; lig++) {
+        for (uint32_t col = 0; col < COL_MAX; col++) {
             // Efface l'écran en écrivant un espace avec les couleurs par défaut (caractère blanc sur fond noir)
             ecrit_car(lig, col, ' ', 15, 0, false);
         }
     }
-    place_curseur(0, 0);
+    ROW = 0;
+    COL = 0;
+    place_curseur(ROW, COL);
+}
+
+/* Fonction qui calcule et positionne le curseur aux coordonnées +1, bornées dans l'écran */
+void place_next_pos_cursor(){
+    if(++COL >= COL_MAX) {
+        COL = 0;
+        if(++ROW >= ROW_MAX) {
+            ROW = 0;
+        }
+    }
+    place_curseur(ROW, COL);
+}
+
+void traite_car(char c)
+{
+    // Switch pour les caractères de contrôle, suivi du placement du curseur
+    switch (c)
+    {
+    case 8: // Backspace
+        ecrit_car(ROW, COL, ' ', 15, 0, false);
+        if(COL > 0) {
+            COL--;
+        } else if(ROW > 0) {
+            ROW--;
+            COL = COL_MAX - 1;
+        }
+        break;
+    case 9: // Tab
+        COL = (COL + 8) & ~(8 - 1); // Avance de 8 positions, aligné sur une tabulation
+        if(COL >= COL_MAX) {
+            COL = 0;
+            ROW++;
+        }
+        break;
+    case 10: // Newline
+        COL = 0;
+        ROW++;
+        if(ROW >= ROW_MAX) {
+            ROW = 0;
+        }
+        break;
+    case 12: // efface l'écran
+        efface_ecran();
+        break;
+    case 13: // Retour chariot
+        COL = 0;
+        break;
+    default:
+        // Comportement par défaut : Afficher le caractère à la position actuelle du curseur
+        ecrit_car(ROW, COL, c, 15, 0, false); // Affiche le caractère avec les couleurs par défaut
+        place_next_pos_cursor();
+        break;
+    }
+    place_curseur(ROW, COL);
 }
