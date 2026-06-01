d07819225aba60a63f6f68c47fb3cf6055a7a8fa
Gabriel Valente <gabriel.valente@grenoble-inp.org>
2026-06-01 10:43:34 +0200

    ecrit_car() OK

--- kernel/console.c
+++ kernel/console.c
@@ -1,8 +1,15 @@
 #include "stdint.h"
+#include "stdbool.h"
 
 const uint16_t* VGABASE = (uint16_t*)0xB8000;
 
 const uint16_t* ptr_mem(uint32_t lig, uint32_t col)
 {
     return VGABASE + (lig * 80 + col);
+}
+
+void ecrit_car(uint32_t lig, uint32_t col, char c, uint8_t ct, uint8_t cf, bool cl)
+{
+    uint16_t* ptr = (uint16_t*)ptr_mem(lig, col);
+    *ptr = (c << 8) | (cl << 7) | (cf << 4) | (ct); 
 }
