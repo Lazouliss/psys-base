65722f3aa8f554483c90e87e5d5ebb14e1d0016e
Gabriel Valente <gabriel.valente@grenoble-inp.org>
2026-06-01 12:02:36 +0000

    Phase1

--- kernel/start.c
+++ kernel/start.c
@@ -16,14 +16,28 @@ void kernel_start(void)
 {
 	int i;
 	// call_debugger(); useless with qemu -s -S
-
 	i = 10;
-
 	i = fact(i);
-	efface_ecran();
+
+
+	/************************/
+	/* Tests simples printf */
+	/************************/
+	//efface_ecran();
+	// Supprime le contenu de l'écran de base
+	printf("\f");
+	// Affiche un simple texte qui disparaitra
 	printf("Hello, World!\n");
+	// Affiche le résultat contenu dans la variable i
 	printf("10! = %d\n", i);
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nderniere lignes");
+	// Test tabulation
+	printf("tab\tulation\n");
+	// Test defilement()
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nderniere lignes");
+	// Test place_curseur() au milieu de l'écran
+	place_curseur(12, 40);
+	// Reviens au début de la ligne, puis écrit 5 caractères, et place le curseur SUR le dernier caractère
+	printf("\r klfd\b");
 	
 	while(1)
 	  hlt();
